770ae5382c22237e6f18d11436eef55de55eba06
Iñigo Crespo <inigo.crespo@mail.com>
2019-04-20 20:55:27 +0200

    feat: shift and altgr

--- main.go
+++ main.go
@@ -9,40 +9,88 @@ import (
 	"unsafe"
 )
 
-type InputEvent struct {
-	Time  syscall.Timeval // 16 bytes
-	Type  uint16          // 2 bytes
-	Code  uint16          // 2 bytes
-	Value int32           // 4 bytes
+type RawKeyEvent struct {
+	Time  syscall.Timeval
+	Type  uint16
+	Code  uint16
+	Value int32
 }
 
-var size uintptr = unsafe.Sizeof(InputEvent{})
+var size uintptr = unsafe.Sizeof(RawKeyEvent{})
 
 func main() {
 
-	kbd, err := os.Open("/dev/input/event0")
+	f, err := os.Open("/dev/input/event0")
 	if err != nil {
 		panic(err)
 		return
 	}
-	defer kbd.Close()
+	defer f.Close()
+
+	fd := int(f.Fd())
+
+	var lshift bool
+	var rshift bool
+	var altgr bool
 
 	for {
+
 		b := make([]byte, size)
-		n, err := kbd.Read(b)
+		_, err := syscall.Read(fd, b)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		ie := &InputEvent{}
-		err = binary.Read(bytes.NewBuffer(b), binary.LittleEndian, ie)
+		e := &RawKeyEvent{}
+		err = binary.Read(bytes.NewBuffer(b), binary.LittleEndian, e)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		fmt.Println(ie)
+		if e.Type == 1 && e.Code == 42 && (e.Value == 1 || e.Value == 2) {
+			lshift = true
+		}
+
+		if e.Type == 1 && e.Code == 42 && e.Value == 0 {
+			lshift = false
+			continue
+		}
+
+		if e.Type == 1 && e.Code == 54 && (e.Value == 1 || e.Value == 2) {
+			rshift = true
+		}
+
+		if e.Type == 1 && e.Code == 54 && e.Value == 0 {
+			rshift = false
+			continue
+		}
+
+		if e.Type == 1 && e.Code == 100 && (e.Value == 1 || e.Value == 2) {
+			altgr = true
+		}
+
+		if e.Type == 1 && e.Code == 100 && e.Value == 0 {
+			altgr = false
+			continue
+		}
+
+		if e.Type == 1 && (e.Value == 1 || e.Value == 2) {
+			if int(e.Code) < len(keys) {
+				if lshift || rshift {
+					fmt.Print(shiftKeys[e.Code])
+				} else if altgr {
+					fmt.Print(altgrtKeys[e.Code])
+				} else {
+					fmt.Print(keys[e.Code])
+				}
+			} else {
+				fmt.Print("<Unknonw ", e.Code, ">")
+			}
+		}
+
+		//fmt.Println("Type ", e.Type, "Code ", e.Code, "Value ", e.Value)
 	}
 
 }
